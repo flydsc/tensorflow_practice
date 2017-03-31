bf2efb7b71fcb3bc20c814830f3122f8856e8471
dsc.rMBP13 <dongshichao@gmail.com>
2017-03-31 21:55:19 +0800

    predict3

--- RNN/sentiment/class_init.py
+++ RNN/sentiment/class_init.py
@@ -14,8 +14,8 @@ n_inputs = max_len
 n_hidden_units = 128
 n_classes = labels_n
 
-x = tf.placeholder(tf.int32, [batch_size, n_inputs])
-y = tf.placeholder(tf.int32, [batch_size, n_classes])
+x = tf.placeholder(tf.int32, [None, n_inputs])
+y = tf.placeholder(tf.int32, [None, n_classes])
 
 #define weights
 
@@ -27,28 +27,7 @@ biases = {
     "out": tf.Variable(0.1, [n_classes,])
 }
 
-def RNN(X, weights, biases):
-    # hidden layer for input to cell
-    ########################################
-    embedding = tf.get_variable('embedding', [word_size, n_hidden_units])
-    inputs = tf.nn.embedding_lookup(embedding, X)
-    # x_in = tf.matmul(inputs, weights['in']) + biases['in']
-
-    # cell
-    ##########################################
-    lstm_cell = tf.contrib.rnn.BasicLSTMCell(n_hidden_units, forget_bias=1.0, state_is_tuple=True)
-    init_state = lstm_cell.zero_state(batch_size, dtype=tf.float32)
-    outputs, final_state = tf.nn.dynamic_rnn(lstm_cell, inputs, initial_state=init_state, time_major=False)
-
-
-
-    # hidden layer for output as the final results
-    #############################################
-    results = tf.matmul(final_state[1], weights['out']) + biases['out']
-    return results
-
-def RNNN(X, weights, biases):
-    batch_size = 1
+def RNN(X, weights, biases, train=True):
     # hidden layer for input to cell
     ########################################
     embedding = tf.get_variable('embedding', [word_size, n_hidden_units])
@@ -70,8 +49,7 @@ def RNNN(X, weights, biases):
 
 
 pred = RNN(x, weights, biases)
-predd = RNNN(x, weights, biases)
-prediction = tf.argmax(predd, 1)
+prediction = tf.argmax(pred, 1)
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=y))
 train_op = tf.train.AdamOptimizer(lr).minimize(cost)
 
@@ -111,9 +89,19 @@ with tf.Session() as sess:
     for t in test_data:
         result.append(
             sess.run([prediction], feed_dict={
-                x:t
+                x: t
             })
         )
+    step = 0
+    test_len = len(test_data)
+    while step * batch_size <= data_len:
+        batch_xt = test_data[(step-1) * batch_size : step * batch_size]
+        result.append(sess.run([prediction], feed_dict={x: batch_xt}))
+    remain = test_len - (step-1) * batch_size
+    start = batch_size - remain
+    batch_xt = test_len[(step-1) * batch_size :] + test_len[:start]
+    final = sess.run([prediction], feed_dict={x: batch_xt})
+    result.append(final[:remain])
     with open('result.txt', 'w') as out:
         for i in result:
             out.write(str(i) + '\n')
